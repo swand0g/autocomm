b1fb48e5640cf80d1d43278adae4fe6bfed1dc7e
swand0g <alexanderjswanson@icloud.com>
2023-02-01 21:49:57 -0500

    add makefile, cleanup, update gitignore

--- main.go
+++ main.go
@@ -7,22 +7,6 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-var netsema = make(chan struct{}, 1)
-
-func main() {
-	os.Setenv("DEBUG", "1")
-	f := setupLogging()
-	if f != nil {
-		defer f.Close()
-	}
-
-	p := tea.NewProgram(InitalModel())
-	if _, err := p.Run(); err != nil {
-		fmt.Printf("Alas, there's been an error: %v", err)
-		os.Exit(1)
-	}
-}
-
 func setupLogging() *os.File {
 	if len(os.Getenv("DEBUG")) > 0 {
 		f, err := tea.LogToFile("debug.log", "debug")
@@ -35,13 +19,13 @@ func setupLogging() *os.File {
 	return nil
 }
 
-func printCommitSugestionsAs() {
-	k, _ := getAPIKey()
-	
-	s, e := fetchCommitSuggestions(k, false)
-	if e != nil {
-		fmt.Println(e)
+func main() {
+	f := setupLogging()
+	if f != nil { defer f.Close() }
+
+	prog := tea.NewProgram(InitalModel())
+	if _, err := prog.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
 	}
-	
-	fmt.Println(s)
 }
